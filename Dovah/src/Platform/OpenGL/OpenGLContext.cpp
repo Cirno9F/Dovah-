916554b0df53ccc220959cccf711d607a374cf5d
Cirno9F <2380604115@qq.com>
2022-11-07 23:35:42 +0800

    Draw Triangle On Screen

--- Dovah/src/Platform/OpenGL/OpenGLContext.cpp
+++ Dovah/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Dovah
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		DOVAH_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		DOVAH_CORE_INFO("OpenGL Info:");
+		DOVAH_CORE_INFO("	Vendor: {0}", glGetString(GL_VENDOR));
+		DOVAH_CORE_INFO("	Renderer: {0}", glGetString(GL_RENDERER));
+		DOVAH_CORE_INFO("	Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
